76a4dfb3dc3b4a3eda8c809651cf025b02796390
Malek Ramadan <mintywicked@gmail.com>
2025-02-13 19:34:03 +0200

    Modifying the the if loop

--- Assignments-1.py
+++ Assignments-1.py
@@ -1,8 +1,10 @@
 age = int(input("Enter your age: "))
 
-if age <= 18:
+if age < 5:
+    print("The ticket price is $0")
+elif age <= 12:
     print("The ticket price is $5")
-elif age >= 19:
+elif age <= 18:
     print("The ticket price is $10")
 else:
     print("The ticket price is $15")
