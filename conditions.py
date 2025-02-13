1e2b39d721e653855328f9708229fc5e7772562d
Malek Ramadan <mintywicked@gmail.com>
2025-02-13 19:08:14 +0200

    Adding Assignments-1

--- conditions.py
+++ conditions.py
@@ -1,29 +1,29 @@
-age = int(input("Enter your age: "))
+# age = int(input("Enter your age: "))
 
-if age > 15:
-    print("You can run this code")
-else:
-    print("You cannot run this code")
+# if age > 15:
+#     print("You can run this code")
+# else:
+#     print("You cannot run this code")
 
-print("Program is done")
+# print("Program is done")
 
-grade = int(input("Enter your grade: "))
+# grade = int(input("Enter your grade: "))
 
-if grade >= 90:
-    print("You got an A")
-elif grade >= 80:
-    print("You got a B")
-elif grade >= 70:
-    print("You got a C")
-elif grade >= 60:
-    print("You got a D")
-else:
-    print("You got an F")
+# if grade >= 90:
+#     print("You got an A")
+# elif grade >= 80:
+#     print("You got a B")
+# elif grade >= 70:
+#     print("You got a C")
+# elif grade >= 60:
+#     print("You got a D")
+# else:
+#     print("You got an F")
 
 
 password = input("Enter your password: ")
 
-if password >= 10:
+if len(password) >= 10:
     print("Long")
 else:
     print("Short")
