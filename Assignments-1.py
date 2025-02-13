0ff0f4f3304beb0d1f56da2d455a0d348ab26c35
Malek Ramadan <mintywicked@gmail.com>
2025-02-13 19:28:58 +0200

    Working on the first assignment

--- Assignments-1.py
+++ Assignments-1.py
@@ -1,4 +1,8 @@
 age = int(input("Enter your age: "))
 
 if age <= 18:
-    print("You are a minor")
+    print("The ticket price is $5")
+elif age >= 19:
+    print("The ticket price is $10")
+else:
+    print("The ticket price is $15")
